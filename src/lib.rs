d803ee21534ed234afe8e2c6a9296747bbbf07c9
Magic Len <len@magiclen.org>
2019-05-01 19:26:55 +0800

    improve API

--- src/lib.rs
+++ src/lib.rs
@@ -9,10 +9,10 @@ See `examples`.
 mod macros;
 
 /// The trait for an authorizer.
-pub trait SimpleAuthorization {
-    /// Check whether the key is valid or not. And a string can also be returned to create an auth instance.
-    fn has_authority<S: AsRef<str>>(key: Option<S>) -> Option<Option<String>>;
+pub trait SimpleAuthorization<E> {
+    /// Check whether the key is valid or not. And a generic type can also be returned to help create an auth instance.
+    fn has_authority<S: AsRef<str>>(key: Option<S>) -> Option<Option<E>>;
 
     /// Create an auth instance.
-    fn create_auth(key: Option<String>) -> Self;
+    fn create_auth(key: Option<E>) -> Self;
 }
--- src/macros.rs
+++ src/macros.rs
@@ -1,7 +1,10 @@
-/// To let a struct which implements the `SimpleAuthorization` trait become an authorizer.
+/// To let a struct which implements the `SimpleAuthorization<E>` trait become an authorizer. The default `<E>` is `<String>`.
 #[macro_export]
 macro_rules! authorizer {
     ( $name:ident ) => {
+        authorizer!($name, String);
+    };
+    ( $name:ident, $tpy:ty ) => {
         impl<'a, 'r> ::rocket::request::FromRequest<'a, 'r> for $name {
             type Error = ();
 
@@ -10,8 +13,8 @@ macro_rules! authorizer {
 
                 let key = keys.into_iter().next();
 
-                match <$name as ::rocket_simple_authorization::SimpleAuthorization>::has_authority(key) {
-                    Some(key) => ::rocket::Outcome::Success(<$name as ::rocket_simple_authorization::SimpleAuthorization>::create_auth(key)),
+                match <$name as ::rocket_simple_authorization::SimpleAuthorization<$tpy>>::has_authority(key) {
+                    Some(key) => ::rocket::Outcome::Success(<$name as ::rocket_simple_authorization::SimpleAuthorization<$tpy>>::create_auth(key)),
                     None => ::rocket::Outcome::Forward(())
                 }
             }
